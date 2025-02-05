dc5df5745e5f147afea1838eba01206cba9adcd8
FCG <fredclamp-gray@Freds-MacBook-Pro-2.local>
2025-02-05 16:49:43 +0000

    test astrocat sketch

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,7 +1,13 @@
-function setup() {
-  createCanvas(1000, 400);
+let astrocat;
+
+function preload() {
+  astrocat = loadimage("astrocat1.png");
 }
 
-function draw() {
-  background(250, 240, 230);
+function setup() {
+  createCanvas(1000, 400);
+  background(18, 20, 41);
+  image(astrocat, 0, 0);
 }
+
+function draw() {}
