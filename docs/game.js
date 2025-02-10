c7f3e10dcd8c95d8a88f2568dbd035f16134ff4b
fred1778 <ey24984@bristol.ac.uk>
2025-02-10 14:39:49 +0000

    update

--- docs/game.js
+++ docs/game.js
@@ -3,32 +3,29 @@ const GameStates = Object.freeze({
 });
 
 class Game {
-  constructor(lvlNum, player1, player2) {
+  constructor(lvlNum, player1) {
     this.lvl_num = lvlNum;
     this.currentRoom = 0;
     this.gameState = GameStates.ACTIVE;
     this.player_1 = player1;
-    this.player_2 = player2;
     this.level = new Level(lvlNum);
+    this.sprites = { player1 };
   }
 
-  update() {
-    this.player_1.update();
-    this.player_2.update();
-    for (m in this.level.currentRoom.mobs) {
-      m.update();
-    }
-    // similar for projectiles
+  registerSprite(sprite) {
+    this.sprites.push(sprite);
   }
+  update() {
+    // similar for projectiles
+    for (let s of this.sprites) {
+      s.update();
+    }
+  }
+
   draw() {
     // Mobs draw
-    this.player_1.draw();
-    this.player_2.draw();
-
-    for (m in this.level.currentRoom.mobs) {
-      m.draw();
+    for (let s of this.sprites) {
+      s.draw();
     }
   }
-
-  handlleInput(key) {}
 }
