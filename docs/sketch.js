f8b15515612fbe1f38fa70e108c0e15514b96a8b
fred1778 <ey24984@bristol.ac.uk>
2025-02-10 14:19:09 +0000

    ddd

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,8 +1,11 @@
 /* GLOBAL CONSTANTS */
 let game;
+let asset_astrocat;
 
 function preload() {
   // preload images in to global vars here
+
+  asset_astrocat = loadImage("./Sprites/astrocat.png");
 }
 
 function setup() {
@@ -13,4 +16,10 @@ function setup() {
 
 function draw() {
   background(10, 11, 28);
+  var sprite = new Sprite(100, 100, 100);
+
+  if (game.currentStatus == GameStatus.ACTIVE) {
+    game.update();
+    game.draw();
+  }
 }
