31a07265c2855c080f3b6925f44a958d8bd7bc12
fred <ey24984@bristol.ac.uk>
2025-03-18 12:00:54 +0000

    ui chnages

--- docs/src/entities/base/Projectile.js
+++ docs/src/entities/base/Projectile.js
@@ -1,48 +1,52 @@
-class Projectile extends GameObject { // maybe extend game object to avoid the health sprite mechanic
+class Projectile extends GameObject {
+  // maybe extend game object to avoid the health sprite mechanic
 
-   constructor(x, y, velocityX, velocityY, lastDirection) {
-      super(x, y);
-      this.position = createVector(x, y);
-      this.velocity = createVector(10 * velocityX, 10 * velocityY);
-      this.lastDirection = lastDirection;
-   }
+  constructor(x, y, velocityX, velocityY, lastDirection) {
+    super(x, y);
+    this.position = createVector(x, y);
+    this.velocity = createVector(10 * velocityX, 10 * velocityY);
+    this.lastDirection = lastDirection;
+  }
 
-   update() {
-      // Deactive the projectile if it leaves the room boundaries for performance reasons ;)
-      if (this.position.x < tileSize || this.position.x > roomWidth * tileSize - tileSize ||
-         this.position.y < tileSize || this.position.y > roomHeight * tileSize - tileSize) {
-         this.isActive = false;
-      }
-      if (this.isActive) {
-         this.position.add(this.velocity);
-      }
-   }
+  update() {
+    // Deactive the projectile if it leaves the room boundaries for performance reasons ;)
+    if (
+      this.position.x < tileSize ||
+      this.position.x > roomWidth * tileSize - tileSize ||
+      this.position.y < tileSize ||
+      this.position.y > roomHeight * tileSize - tileSize
+    ) {
+      this.isActive = false;
+    }
+    if (this.isActive) {
+      this.position.add(this.velocity);
+    }
+  }
 
-   draw() {
-      if (this.isActive) {
-         // Proper circle rendering with fill and stroke
-         push();
-         translate(this.position.x, this.position.y);
-         imageMode(CENTER);
-         //not used yet, trying to rotate the bullets by 45 degrees depending on the angle
-         // const directionAngles = {
-         //     "UP": -90,
-         //     "DOWN": 90,
-         //     "LEFT": 180,
-         //     "RIGHT": 0,
-         //     "UP-RIGHT": -45,
-         //     "UP-LEFT": -135,
-         //     "DOWN-RIGHT": 45,
-         //     "DOWN-LEFT": 135,
-         // };
+  draw() {
+    if (this.isActive) {
+      // Proper circle rendering with fill and stroke
+      push();
+      translate(this.position.x, this.position.y);
+      imageMode(CENTER);
+      //not used yet, trying to rotate the bullets by 45 degrees depending on the angle
+      // const directionAngles = {
+      //     "UP": -90,
+      //     "DOWN": 90,
+      //     "LEFT": 180,
+      //     "RIGHT": 0,
+      //     "UP-RIGHT": -45,
+      //     "UP-LEFT": -135,
+      //     "DOWN-RIGHT": 45,
+      //     "DOWN-LEFT": 135,
+      // };
 
-         //
-         // let angle = directionAngles[this.lastDirection] || 0; // Default to 0 if not found
-         // rotate(radians(angle));
+      //
+      // let angle = directionAngles[this.lastDirection] || 0; // Default to 0 if not found
+      // rotate(radians(angle));
 
-         image(bullet, 0, 0, 150, 150);
-         pop();
-      }
-   }
-
+      image(bullet, 0, 0, 16, 16);
+      pop();
+    }
+  }
 }
